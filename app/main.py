0e0614465b17962e889c89579d1bbd77f53d5fde
yahayaohinoyi <yahaya.suleiman2204@gmail.com>
2022-01-08 00:07:10 +0000

    feat: added cat-file functionality

--- app/main.py
+++ app/main.py
@@ -1,12 +1,11 @@
 import sys
 import os
+import zlib
 
 
 def main():
     print("Your code goes here!")
-
     # Uncomment this block to pass the first stage
-    
     command = sys.argv[1]
     if command == "init":
         os.mkdir(".git")
@@ -15,8 +14,24 @@ def main():
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/master\n")
         print("Initialized git directory")
+
+    elif command == "cat-file":
+        argType = sys.argv[2]
+        sha = sys.argv[3]
+        cat_file(argType, sha)
     else:
         raise RuntimeError(f"Unknown command #{command}")
+
+def cat_file(argType, sha):
+    if argType == "-p":
+        # decompressed_blob = zlib.decompress(sha)
+        file = f".git/objects/{sha[:2]}/{sha[2:]}"
+        # print(file)
+        with open(file, "rb") as f:
+            decompressed_blob = zlib.decompress(f.read())
+            print(decompressed_blob)
+        return decompressed_blob
+
 
 
 if __name__ == "__main__":
