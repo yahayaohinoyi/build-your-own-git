6e5dc5128a6bdd0d58a577c187ef5f12d2afaf78
yahayaohinoyi <yahaya.suleiman2204@gmail.com>
2022-01-08 01:12:43 +0000

    fix: removed all non ascii characters

--- app/main.py
+++ app/main.py
@@ -25,10 +25,10 @@ def cat_file(argType, sha):
     if argType == "-p":
         file = f".git/objects/{sha[:2]}/{sha[2:]}"
         with open(file, "rb") as f:
-            decompressed_blob = zlib.decompress(f.read()).decode()
+            decompressed_blob = zlib.decompress(f.read()).decode('utf8')
             data = decompressed_blob.split()
             objectType, fileContent = data[0], ' '.join(data[1:])
-            print(fileContent[2:])
+            print(fileContent)
         return decompressed_blob
 
 
